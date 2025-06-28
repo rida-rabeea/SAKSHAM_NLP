c1207bcfcc5b7b7fb8dd2cd132ed4d6be6ce46ad
rida-rabeea <rabeearida@gmail.com>
2025-06-28 20:52:42 +0530

    Update model_loader.py

--- model_loader.py
+++ model_loader.py
@@ -3,6 +3,23 @@ import pandas as pd
 from transformers import BartTokenizer, BartForSequenceClassification
 from pymongo import MongoClient
 from dotenv import load_dotenv
+import zipfile
+import requests
+
+def download_and_unzip_from_drive(file_id, destination_folder):
+    url = f"https://drive.google.com/uc?export=download&id=1V-sixHIF2uxjPMyVdL6YuN3Me-6Lm2hi"
+    zip_path = "model.zip"
+
+    # Download ZIP
+    response = requests.get(url)
+    with open(zip_path, "wb") as f:
+        f.write(response.content)
+
+    # Unzip it
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(destination_folder)
+
+    os.remove(zip_path)  # Clean up
 
 def load_model_and_resources():
     load_dotenv()
