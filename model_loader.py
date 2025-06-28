9f367a774477282770648598e07caf7832600ae1
rida-rabeea <rabeearida@gmail.com>
2025-06-28 20:55:30 +0530

    Update model_loader.py

--- model_loader.py
+++ model_loader.py
@@ -1,10 +1,10 @@
 import os
 import pandas as pd
+import zipfile
+import requests
 from transformers import BartTokenizer, BartForSequenceClassification
 from pymongo import MongoClient
 from dotenv import load_dotenv
-import zipfile
-import requests
 
 def download_and_unzip_from_drive(file_id, destination_folder):
     url = f"https://drive.google.com/uc?export=download&id=1V-sixHIF2uxjPMyVdL6YuN3Me-6Lm2hi"
@@ -23,12 +23,20 @@ def download_and_unzip_from_drive(file_id, destination_folder):
 
 def load_model_and_resources():
     load_dotenv()
-    
-    model = BartForSequenceClassification.from_pretrained("./final_model")
-    tokenizer = BartTokenizer.from_pretrained("./final_model")
-    df = pd.read_csv("intent_mappings.csv",encoding="ISO-8859-1")
-    
+
+    model_dir = "final_model"
+
+    # Extract ID from Google Drive link in .env
+    if not os.path.exists(model_dir):
+        drive_link = os.getenv("DRIVE_FILE_ID")
+        file_id = drive_link.split("/")[5] if "drive.google.com" in drive_link else drive_link
+        download_and_unzip_from_drive(file_id, model_dir)
+
+    model = BartForSequenceClassification.from_pretrained(model_dir)
+    tokenizer = BartTokenizer.from_pretrained(model_dir)
+    df = pd.read_csv("intent_mappings.csv", encoding="ISO-8859-1")
+
     mongo_client = MongoClient(os.getenv("MONGO_URI"))
     collection = mongo_client["ISMDATA"]["ISMDATA"]
-    
+
     return model, tokenizer, df, collection
